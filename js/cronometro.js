bbd6d8c0634d3d354c7e620fab7040733021f2b0
Ángel Macías <uo289362@uniovi.es>
2025-10-30 14:53:51 +0100

    ejercicio 1 - p7-ecmascript

---
+++ js/cronometro.js
@@ -0,0 +1,54 @@
+"use strict";
+
+class Cronometro {
+  constructor() {
+    this.tiempo = 0;
+  }
+
+  arrancar() {
+    try {
+      this.inicio = Temporal.Now.instant().epochMilliseconds; // Returns the current time in milliseconds.
+    } catch (error) {
+      console.error("Error al arrancar el cronómetro:", error);
+      this.inicio = Date.now(); // Returns the time in milliseconds
+    }
+
+    console.log("Cronómetro arrancado en:", this.inicio);
+
+    this.corriendo = setInterval(this.actualizar.bind(this), 100);
+  }
+
+  actualizar() {
+    var now = Date.now();
+
+    this.tiempo = now - this.inicio;
+
+    this.mostrar();
+  }
+
+  mostrar() {
+    // mm:ss.s
+    const minutos = parseInt(this.tiempo / 60000);
+    const segundos = ((this.tiempo % 60000) / 1000).toFixed(1);
+
+    const dateString = `${String(minutos).padStart(2, "0")}:${String(
+      segundos
+    ).padStart(4, "0")}`;
+
+    const parrafoCronometro = document.querySelector("main p");
+
+    parrafoCronometro.textContent = dateString;
+  }
+
+  parar() {
+    clearInterval(this.corriendo);
+  }
+
+  reiniciar() {
+    this.parar();
+
+    this.tiempo = 0;
+
+    this.mostrar();
+  }
+}
