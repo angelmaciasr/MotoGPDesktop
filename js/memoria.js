646bbd1aaf8cd3166a0347ccf10e0a3d28268364
Ángel Macías <uo289362@uniovi.es>
2025-10-30 23:56:55 +0100

    ejercicio 2 de p7-ecmascript

--- js/memoria.js
+++ js/memoria.js
@@ -1,9 +1,57 @@
 "use strict";
 
 class Memoria {
-  constructor() {}
+  constructor() {
+    this.tablero_bloqueado = true;
+    this.primera_carta = null;
+    this.segunda_carta = null;
+
+    this.barajarCartas();
+  }
 
   voltearCarta(carta) {
     carta.dataset.estado = "volteada";
+  }
+
+  barajarCartas() {
+    const main = document.querySelector("main");
+    const cartas = Array.from(document.querySelectorAll("main article"));
+
+    for (let i = cartas.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [cartas[i], cartas[j]] = [cartas[j], cartas[i]];
+    }
+
+    cartas.forEach((carta) => {
+      main.appendChild(carta);
+    });
+  }
+
+  reiniciarAtributos() {
+    this.tablero_bloqueado = true;
+    this.primera_carta = null;
+    this.segunda_carta = null;
+  }
+
+  deshabilitarCartas() {
+    this.primera_carta.dataset.estado = "revelada";
+
+    this.segunda_carta.dataset.estado = "revelada";
+
+    this.reiniciarAtributos();
+
+    this.comprobarJuego();
+  }
+
+  comprobarJuego() {
+    const cartas = document.querySelectorAll("main article");
+
+    Array.from(cartas).forEach((carta) => {
+      if (carta.dataset.estado !== "revelada") {
+        return;
+      }
+    });
+
+    console.log("¡Felicidades! Has ganado el juego.");
   }
 }
